9e299ed4bb2a8deedb864d5ae374a0e7a9146126
Gal Amouyal <gal.amouyal@ibm.com>
2020-12-09 20:05:20 +0200

    jsx elemetns

--- src/App.js
+++ src/App.js
@@ -5,9 +5,27 @@ import "./App.css";
 const userNameSpan = <h2> Gal Amouyal </h2>;
 
 const header = "Galamouyal@gmail.com";
+// Jsx elements can be placed inisde array
+// const arrayOfHeaders = [header, header, header];
 
 function App() {
-  return <div className="App">{header}</div>;
+  const student2 = "Noam";
+  const numberOfStudents = 5;
+  const student3 = <li> Haim </li>;
+
+  return (
+    <div className="App">
+      <div>{header}</div>
+      <h2>
+        Students <span> {numberOfStudents} </span>
+      </h2>
+      <ul>
+        <li> Sapir </li>
+        <li> {student2} </li>
+        {student3}
+      </ul>
+    </div>
+  );
 }
 
 function HeaderComponent() {
