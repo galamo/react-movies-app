fd05a801977871084021cec85bc191e34207b980
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2021-01-17 18:52:28 +0200

    client

--- src/redux-store/async/getMovieDetails.async.js
+++ src/redux-store/async/getMovieDetails.async.js
@@ -10,8 +10,9 @@ const { dispatch } = store;
 export async function getMovieDetails(id) {
 
     try {
-        const { data } = await axios.get(`${API_URL}&i=${id}`)
-        dispatch({ type: ACTIONS.MOVIES.GET_MOVIE_DETAILS_DONE, payload: data })
+        const { data } = await axios.get(`${API_URL}/${id}`)
+        const { byId } = data;
+        dispatch({ type: ACTIONS.MOVIES.GET_MOVIE_DETAILS_DONE, payload: byId[0] })
     } catch (ex) {
 
         console.log("we had an error", ex)
--- src/redux-store/async/getMovies.async.js
+++ src/redux-store/async/getMovies.async.js
@@ -2,7 +2,7 @@
 import axios from "axios"
 import { ACTIONS } from "../actions";
 import store from "../index"
-export const API_URL = "http://www.omdbapi.com/?plot=full&apikey=4f7462e2"
+export const API_URL = "http://localhost:5000/movies"
 const { dispatch } = store;
 
 export async function getMovies(inputValue, searchBy) {
@@ -10,14 +10,10 @@ export async function getMovies(inputValue, searchBy) {
         if (!inputValue) return;
         //  SET LOADING
 
-        const { data } = await axios.get(`${API_URL}&${searchBy}=${inputValue}`)
-        const { Response, Error: errorMessage } = data;
-        if (Response === "False") {
-            // ERROR ACTION
-        } else {
-            console.log("movies arrived")
-            const dataFromServer = data.Search || [data];
-            dispatch({ type: ACTIONS.MOVIES.GET_FROM_SERVER_DONE, payload: dataFromServer })
+        const { data } = await axios.get(`${API_URL}/search?searchBy=${searchBy}&inputValue=${inputValue}`)
+        const { results } = data;
+        if (results && results.length) {
+            dispatch({ type: ACTIONS.MOVIES.GET_FROM_SERVER_DONE, payload: results })
         }
     } catch (ex) {
         // ERROR ACTION
